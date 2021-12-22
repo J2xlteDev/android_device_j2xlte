6a0be9d2cf0b8c94cee13ab91b05fca067db85c5
NullCode <ishaqahmed.dev@gmail.com>
2021-12-22 16:58:28 +0600

    Add debug.sh in rootdir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,15 @@ LOCAL_PATH:= $(call my-dir)
 
 # Init scripts
 
+# Logging
+include $(CLEAR_VARS)
+LOCAL_MODULE       := debug.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/debug.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.sc8830
 LOCAL_MODULE_TAGS  := optional eng
